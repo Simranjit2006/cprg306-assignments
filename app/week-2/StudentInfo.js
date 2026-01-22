b469f2c10e3e8d6c0b6147f1310cea2423198ab1
Simranjit2006 <simranjit.singh2@edu.sait.ca>
2026-01-22 13:13:02 -0700

    changes in file

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+ 
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>Simranjit Singh</h1>
+ 
+      <p>
+        <Link href="https://github.com/your-username/your-repository">
+          GitHub Repository
+        </Link>
+      </p>
+    </div>
+  );
+}
