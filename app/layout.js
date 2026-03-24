28acd3b2c5191b00ea87e95b44750b72939129a8
Simranjit2006 <simranjit.singh2@edu.sait.ca>
2026-03-24 00:17:39 -0600

    .

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AuthContextProvider } from "./contexts/AuthContext.js";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,8 +22,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      > <AuthContextProvider>
+          {children}
+        </AuthContextProvider>;
       </body>
     </html>
   );
